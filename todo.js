9df79aeb2d5ef50fef365233e82aaef7659959f6
henryho173 <henryho173@gmail.com>
2022-11-06 13:55:19 +1100

    Update todo.js

--- todo.js
+++ todo.js
@@ -95,15 +95,52 @@ document.addEventListener('DOMContentLoaded', () => {
   }
 });
 
-showCharacters = characters => {
-  const charactersDiv = document.querySelector(‘#rick-and-morty-  characters’);
-  characters.forEach(character => {
-    const characterElement = document.createElement(‘p’);
-    characterElement.innerText = `Character Name: ${character.name}`;
-    charactersDiv.append(characterElement);
-  });
-}
+fetch("https://www.thecocktaildb.com/api/json/v1/1/random.php")
+  .then((response) => {
+    if (response.ok) {
+      return response.json();
+    } else {
+      throw new Error("NETWORK RESPONSE ERROR");
+    }
+  })
+  .then(data => {
+    console.log(data);
+    displayCocktail(data)
+  })
+  .catch((error) => console.error("FETCH ERROR:", error));
 
-fetch(‘https://rickandmortyapi.com/api/character/’)
-   .then(response => response.json())
-   .then(characters => showCharacters(characters.results));
+function displayCocktail(data) {
+  const cocktail = data.drinks[0];
+  const cocktailDiv = document.getElementById("cocktail");
+  ...  
+}
+  const cocktailName = cocktail.strDrink;
+  const heading = document.createElement("h1");
+  heading.innerHTML = cocktailName;
+  cocktailDiv.appendChild(heading);
+  
+  const cocktailImg = document.createElement("img");
+  cocktailImg.src = cocktail.strDrinkThumb;
+  cocktailDiv.appendChild(cocktailImg);
+  document.body.style.backgroundImage = "url('" + cocktail.strDrinkThumb + "')";
+  
+  const cocktailIngredients = document.createElement("ul");
+  cocktailDiv.appendChild(cocktailIngredients);  
+  
+  const getIngredients = Object.keys(cocktail)
+    .filter(function (ingredient) {
+      return ingredient.indexOf("strIngredient") == 0;
+    })
+    .reduce(function (ingredients, ingredient) {
+      if (cocktail[ingredient] != null) {
+        ingredients[ingredient] = cocktail[ingredient];
+      }
+      return ingredients;
+    }, {});
+
+  for (let key in getIngredients) {
+    let value = getIngredients[key];
+    listItem = document.createElement("li");
+    listItem.innerHTML = value;
+    cocktailIngredients.appendChild(listItem);
+  }
