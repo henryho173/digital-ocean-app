83ee6e28576c09f62ba9cfd1219b01031ae3878e
henryho173 <henryho173@gmail.com>
2022-11-06 15:30:38 +1100

    Update todo.js

--- todo.js
+++ todo.js
@@ -108,39 +108,3 @@ fetch("https://www.thecocktaildb.com/api/json/v1/1/random.php")
     displayCocktail(data)
   })
   .catch((error) => console.error("FETCH ERROR:", error));
-
-const cocktail = data.drinks[0];
-
-function displayCocktail(data) {
-  const cocktailDiv = document.getElementById("cocktail")
-  const cocktailName = cocktail.strDrink;
-  const heading = document.createElement("h1");
-  heading.innerHTML = cocktailName;
-  cocktailDiv.appendChild(heading);
-  };
-  
-  const cocktailImg = document.createElement("img");
-  cocktailImg.src = cocktail.strDrinkThumb;
-  cocktailDiv.appendChild(cocktailImg);
-  document.body.style.backgroundImage = "url('" + cocktail.strDrinkThumb + "')";
-  
-  const cocktailIngredients = document.createElement("ul");
-  cocktailDiv.appendChild(cocktailIngredients);  
-  
-  const getIngredients = Object.keys(cocktail)
-    .filter(function (ingredient) {
-      return ingredient.indexOf("strIngredient") == 0;
-    })
-    .reduce(function (ingredients, ingredient) {
-      if (cocktail[ingredient] != null) {
-        ingredients[ingredient] = cocktail[ingredient];
-      }
-      return ingredients;
-    }, {});
-
-  for (let key in getIngredients) {
-    let value = getIngredients[key];
-    listItem = document.createElement("li");
-    listItem.innerHTML = value;
-    cocktailIngredients.appendChild(listItem);
-  }
