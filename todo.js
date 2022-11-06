3995da537a3ceb991b18fa8dbea796339e904d70
henryho173 <henryho173@gmail.com>
2022-11-06 14:02:54 +1100

    Update todo.js

--- todo.js
+++ todo.js
@@ -111,8 +111,7 @@ fetch("https://www.thecocktaildb.com/api/json/v1/1/random.php")
 
 function displayCocktail(data) {
   const cocktail = data.drinks[0];
-  const cocktailDiv = document.getElementById("cocktail");
-  ...  
+  const cocktailDiv = document.getElementById("cocktail")
 };
   const cocktailName = cocktail.strDrink;
   const heading = document.createElement("h1");
