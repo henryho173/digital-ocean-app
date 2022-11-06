acecbdf4af2989a8572ad95e606efbadb3a7e0b7
henryho173 <henryho173@gmail.com>
2022-11-06 20:05:45 +1100

    Update todo.js

--- todo.js
+++ todo.js
@@ -110,18 +110,26 @@ fetch("https://www.thecocktaildb.com/api/json/v1/1/random.php")
   .catch((error) => console.error("FETCH ERROR:", error));
 
 function displayCocktail(data) {
+
   const cocktail = data.drinks[0];
-  const cocktailDiv = document.getElementById("cocktail");
+  const cocktailDiv = document.getElementById("cocktail");    
+
+  // cocktail name
   const cocktailName = cocktail.strDrink;
   const heading = document.createElement("h1");
   heading.innerHTML = cocktailName;
   cocktailDiv.appendChild(heading);
+
+  // cocktail image
   const cocktailImg = document.createElement("img");
   cocktailImg.src = cocktail.strDrinkThumb;
   cocktailDiv.appendChild(cocktailImg);
   document.body.style.backgroundImage = "url('" + cocktail.strDrinkThumb + "')";
-    const cocktailIngredients = document.createElement("ul");
-  cocktailDiv.appendChild(cocktailIngredients);  
+
+  // coctail ingredients
+  const cocktailIngredients = document.createElement("ul");
+  cocktailDiv.appendChild(cocktailIngredients);
+
   const getIngredients = Object.keys(cocktail)
     .filter(function (ingredient) {
       return ingredient.indexOf("strIngredient") == 0;
@@ -139,4 +147,5 @@ function displayCocktail(data) {
     listItem.innerHTML = value;
     cocktailIngredients.appendChild(listItem);
   }
-}  
+
+}
